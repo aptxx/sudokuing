530c5b81b98f9b514e4fbc3987b337210a6857d2
Hank <prestorpan@gmail.com>
2024-04-17 00:01:40 +0800

    update loading style
    Signed-off-by: Hank <prestorpan@gmail.com>

--- web/src/components/board/Loading.tsx
+++ web/src/components/board/Loading.tsx
@@ -5,7 +5,7 @@ export default function Loading({ open }: { open: boolean }) {
     <Overlay open={open}>
       <div className="mx-16 w-full transform overflow-hidden rounded-lg bg-white px-4 py-4 shadow-xl transition-all dark:bg-gray-800 ">
         <div className="text-center">
-          <p className="text-lg font-bold text-gray-500 dark:text-gray-300">Loading..</p>
+          <p className="text-lg text-gray-900 dark:text-gray-100">Loading..</p>
         </div>
       </div>
     </Overlay>
