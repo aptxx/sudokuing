240320755e1fadc77a2b7d469bac5afc5863313d
Hank <prestorpan@gmail.com>
2024-04-27 20:56:16 +0800

    add more banner size
    Signed-off-by: Hank <prestorpan@gmail.com>

--- web/src/components/sudoku/Sudoku.tsx
+++ web/src/components/sudoku/Sudoku.tsx
@@ -366,8 +366,9 @@ export default function Sudoku({
         className="flex max-h-[90px] w-full items-center justify-center overflow-hidden"
         id="div-gpt-ad-0"
         adunit={DFP_BANNER_ADUNIT}
-        sizes={[[468, 60], [728, 90], 'fluid']}
+        sizes={[[970, 90], [468, 60], [728, 90], 'fluid']}
         sizeMapping={[
+          { viewport: [1200, 600], sizes: [[970, 90], [468, 60], [728, 90], 'fluid'] },
           { viewport: [1000, 600], sizes: [[468, 60], [728, 90], 'fluid'] },
           { viewport: [700, 400], sizes: [[468, 60], 'fluid'] },
           { viewport: [0, 0], sizes: ['fluid'] },
